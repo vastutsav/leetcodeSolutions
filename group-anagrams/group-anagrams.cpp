2bfebb3d45a6996969d7a8a8bb99854721b209a7
vastutsav <utsavbarman@gmail.com>
2021-08-13 01:28:58 +0530

    Update group-anagrams.cpp
    Used reference variables, reserve and move semantics to improve performance.

--- group-anagrams/group-anagrams.cpp
+++ group-anagrams/group-anagrams.cpp
@@ -3,13 +3,14 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map <string, vector<string>> umap;
         vector <vector<string>> ans;
-        for (string w:strs) {
+        for (string& w:strs) {
             string tmp = w;
             sort(tmp.begin(), tmp.end());
             umap[tmp].push_back(w);
         }
-        for (auto itr = umap.begin(); itr != umap.end(); ++itr) {
-            ans.push_back(itr->second);
+        ans.reserve(umap.size());
+        for (auto& item:umap) {
+            ans.push_back(move(item.second));
         }
         return ans;
         
